b6aed5ea5a208e76b0afb757d56da1458737d286
Felipe Costa <felipecostasiqu@gmail.com>
2023-10-27 10:14:00 -0300

    js radar

---
+++ ex008/index.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`a velocidade do seu carro é ${vel}`)
+
+if(vel > 60){
+    console.log(`Você foi MULTADO sua velocidade é acima da permitida`)
+}else{
+console.log(`Dirija sempre usando cinto de segurança`)
+}
