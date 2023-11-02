9ba7c7c73c2044cbb31af6c3997075c08a0046b0
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-01 22:25:49 -0300

    erro

--- ex015/script.js
+++ ex015/script.js
@@ -1,22 +1,11 @@
-function carregar(){
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagens')
-var data = new Date()
-var hora = data.getHours()
-msg.innerHTML = `<h2>Agora são ${hora} horas</h2>` 
-if (hora >= 0 && hora < 12){
-    //Bom dia
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('{ERROR} Verifique os dados e tente novamente!')
+    }
+
     
-    img.scr = 'fotomanha.jpg'
-}else if(hora >= 12 && hora < 18){  
-    img.src = 'fototarde.jpg'
-    document.body.style.background = '#DFBE72'
-    //Boa tarde
-}else{
-    img.src = 'fotonoite.jpg'
-    document.body.style.background = '#002778'
-    //Boa noite
 }
-
-}
-
