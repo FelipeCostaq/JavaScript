bb592bfafc674ada7a87b29d8f87aae8c840559b
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-08 11:31:18 -0300

    gg

--- ex016/script.js
+++ ex016/script.js
@@ -1,26 +1,11 @@
 function contar(){
-    var ini = window.document.getElementById("inicio")
-    var fim = window.document.getElementById("fim")
-    var pas = document.getElementById("passo")
-    var res = document.getElementById("res")
-    var n1 = Number(ini.value)
-    var n2 = Number(fim.value)
-    var n3 = Number(pas.value)
-    res.innerHTML = `Contando:<br>`
-    
-    if(ini.value.length == 0 || Number(fim.value) <= Number(ini.value) || Number(ini.value) < 0){
-        alert("[ERRO] Algo deu errado! Preencha o campo de forma correta!")
-        res.innerHTML = `Impossível Contar!`
-    }else if(fim.value.length == 0  || Number(fim.value) <= 0){
-        alert("[ERRO] Algo deu errado! Preencha o campo de forma correta!")
-        res.innerHTML = "Impossível Contar!"
-    }else if(pas.value.length == 0 || Number(pas.value) <= 0){
-        alert("[ERRO] PASSO INVÁLIDO! O valor será reajustado para 1!")
-        pas.value = 1
-        res.innerHTML = "Tente Novamente!"
-    }else{
-        for(var n1;n1<=n2;n1 += n3){
-            res.innerHTML += `${n1},`
-         }
+    var ini = document.getElementById('inicio')
+    var fi = document.getElementById('fim')
+    var pas = document.getElementById('passo')
+    var resultado = document.getElementById('res')
+
+
+    if (ini.value.lenght == 0 || fi.value.lenght == 0 || pas.value.lenght == 0  ){
+        alert('ERRO! Faltam Dados.')
     }
-    }
+}
