6c592440db1df99571f914d9650fc39d3eda0211
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-08 14:54:22 -0300

    g

--- ex016/script.js
+++ ex016/script.js
@@ -1,11 +1,22 @@
 function contar(){
-    var ini = document.getElementById('inicio')
-    var fi = document.getElementById('fim')
-    var pas = document.getElementById('passo')
-    var resultado = document.getElementById('res')
+    var inicio = document.getElementById('txti');
+    var fim = document.getElementById('txtf');
+    var passo = document.getElementById('txtp');
+    var res = document.getElementById('res')
 
 
-    if (ini.value.lenght == 0 || fi.value.lenght == 0 || pas.value.lenght == 0  ){
-        alert('ERRO! Faltam Dados.')
+    if (inicio.length == 0 || fim.length == 0 || passo.length == 0  ){
+        alert('ERRO! Faltam Dados.');
+    }else{
+        res.innerHTML = 'Contando...'
+       var i = Number(inicio.value)
+       var f = Number(fim.value)
+       var p = Number(passo.value)
+
+       for(var c = i; c <= f; c+= p){
+        res.innerHTML += ` ${ c } \u{1F449}`
+       }
+       res.innerHTML += `\u{1F3C1}`
     }
 }
+
