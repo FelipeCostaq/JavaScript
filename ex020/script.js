81870db1e7950ad85b383ca54f4ba51dc7084d6c
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-14 09:48:20 -0300

    ooko

--- ex020/script.js
+++ ex020/script.js
@@ -1,10 +1,25 @@
-var num = document.getElementById('nume')
-var nu = Number(num.value)
-var sel = document.getElementById('listanum')
-function add(){
-if(nume.value.length == 0){
-    alert('Coloque um número')
-}else{
-    sel == nu
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
 }
+
+function inLista(n,l){
+    if (l.indexOf(Number(n)) !== -1){
+        return true
+    } else {
+        return false
+    }
+}
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value, valores)){
+
+    }else{
+        alert('Valor invalido ou ja tem na lista')
+    }
 }
