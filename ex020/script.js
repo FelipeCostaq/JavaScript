96aa2f46225e478877eefa1ebba3958bf8ef338e
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-14 09:08:02 -0300

    ex020

---
+++ ex020/script.js
@@ -0,0 +1,10 @@
+var num = document.getElementById('nume')
+var nu = Number(num.value)
+var sel = document.getElementById('listanum')
+function add(){
+if(nume.value.length == 0){
+    alert('Coloque um número')
+}else{
+    sel == nu
+}
+}
