c0ef48865b818ad07dbff5e819a6ad998be4dafd
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-15 09:05:46 -0300

    QWEQWRE

--- ex020/script.js
+++ ex020/script.js
@@ -27,9 +27,36 @@ function adicionar() {
         let item = document.createElement('option');
         item.text = `Valor ${num.value} adicionado.`;
         lista.appendChild(item);
+        res.innerHTML = ``
     } else {
-        alert('Valor inválido ou já encontrado na lista.');
+        alert('[ERROR] Valor inválido ou já encontrado na lista.');
     }
     num.value = '';
 
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        alert('[ERROR] Nenhum valor detectado.')
+    }else{
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if(valores[pos]< menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = ``
+        res.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi o ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi o ${menor}</p>`
+        res.innerHTML += `<p>A soma deles todos é ${soma}</p>`
+        res.innerHTML += `<p>A média deles todos é ${media.toFixed(2)}</p>`
+    }
 }
